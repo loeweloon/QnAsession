8613edf921d6f93785ec797b3504ba2f861109fe
loeweloon <69837330+loeweloon@users.noreply.github.com>
2025-05-24 16:29:39 +0800

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <main className="p-8 text-white bg-black min-h-screen">
-      <h1 className="text-3xl font-bold">Home</h1>
-      <p>This is a placeholder homepage.</p>
-    </main>
-  )
-}
+export { default } from "./display/page"
